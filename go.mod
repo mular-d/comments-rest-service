b9de18db09a3e561ed25c3d78e67c7706c1fab27
Muluneh Derese <muluneh.derese@protonmail.com>
2023-03-19 16:04:57 +0300

    feat: add unit test

--- go.mod
+++ go.mod
@@ -3,6 +3,7 @@ module github.com/targerian1999/comments
 go 1.20
 
 require (
+	github.com/davecgh/go-spew v1.1.1 // indirect
 	github.com/dgrijalva/jwt-go v3.2.0+incompatible // indirect
 	github.com/go-playground/locales v0.14.1 // indirect
 	github.com/go-playground/universal-translator v0.18.1 // indirect
@@ -14,10 +15,13 @@ require (
 	github.com/jmoiron/sqlx v1.3.5 // indirect
 	github.com/leodido/go-urn v1.2.1 // indirect
 	github.com/lib/pq v1.10.7 // indirect
+	github.com/pmezard/go-difflib v1.0.0 // indirect
 	github.com/satori/go.uuid v1.2.0 // indirect
 	github.com/sirupsen/logrus v1.9.0 // indirect
+	github.com/stretchr/testify v1.8.2 // indirect
 	go.uber.org/atomic v1.7.0 // indirect
 	golang.org/x/crypto v0.5.0 // indirect
 	golang.org/x/sys v0.4.0 // indirect
 	golang.org/x/text v0.6.0 // indirect
+	gopkg.in/yaml.v3 v3.0.1 // indirect
 )
---
+++ internal/db/comment_test.go
@@ -0,0 +1,53 @@
+//go:build integration
+// +build integration
+
+package db
+
+import (
+	"context"
+	"fmt"
+	"testing"
+
+	"github.com/stretchr/testify/assert"
+	"github.com/targerian1999/comments/internal/comment"
+)
+
+func TestCommentDatabase(t *testing.T) {
+	t.Run("test create comment", func(t *testing.T) {
+		db, err := NewDatabase()
+		assert.NoError(t, err)
+
+		cmt, err := db.PostComment(context.Background(), comment.Comment{
+			Slug:   "slug",
+			Author: "author",
+			Body:   "body",
+		})
+
+		assert.NoError(t, err)
+
+		newCmt, err := db.GetComment(context.Background(), cmt.ID)
+		assert.NoError(t, err)
+		assert.Equal(t, "slug", newCmt.Slug)
+
+		fmt.Println("testing the creation of comments")
+	})
+
+	t.Run("test delete comment", func(t *testing.T) {
+		db, err := NewDatabase()
+		assert.NoError(t, err)
+
+		cmt, err := db.PostComment(context.Background(), comment.Comment{
+			Slug:   "slug",
+			Author: "me",
+			Body:   "body",
+		})
+
+		assert.NoError(t, err)
+
+		err = db.DeleteComment(context.Background(), cmt.ID)
+		assert.NoError(t, err)
+
+		_, err = db.GetComment(context.Background(), cmt.ID)
+		assert.Error(t, err)
+	})
+}
